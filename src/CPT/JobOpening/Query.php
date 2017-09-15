8de85d11f793e91afa3a6f66a24882394efdac26
Sven van Hal <sven@svenvanhal.nl>
2017-09-15 14:43:51 +0200

    Don't query job openings for inactive or non-existent company IDs.

--- src/CPT/JobOpening/Query.php
+++ src/CPT/JobOpening/Query.php
@@ -15,20 +15,33 @@ class Query
     static function register_hooks()
     {
         is_admin() && add_action('pre_get_posts', [__CLASS__, 'admin_filter']);
-        add_action('pre_get_posts', [__CLASS__, 'randomize_archive']);
+        add_action('pre_get_posts', [__CLASS__, 'job_opening_query']);
         add_filter('query_vars', [__CLASS__, 'custom_query_vars']);
     }
 
     /**
      * Alter main query.
      */
-    static function randomize_archive(\WP_Query $query)
+    static function job_opening_query(\WP_Query $query)
     {
 
-        if (! $query->is_admin && $query->is_main_query() && $query->is_post_type_archive('job_opening')) {
+        if (! $query->is_admin && $query->is_main_query() && $query->get('post_type', false) === 'job_opening') {
 
-            $query->set('orderby', 'rand');
-            $query->set('posts_per_page', -1);
+            // All job openings depend on company
+            $query->set('meta_query', [
+                [
+                    'key' => '_company_id',
+                    'value' => static::_get_active_companies(),
+                    'compare' => 'IN',
+                ],
+            ]);
+
+            // Randomize archives
+            if ($query->is_post_type_archive()) {
+
+                $query->set('orderby', 'rand');
+                $query->set('posts_per_page', -1);
+            }
         }
 
         return $query;
@@ -64,5 +77,23 @@ class Query
         }
 
         return $query;
+    }
+
+    /**
+     * Based on get_all_page_ids().
+     *
+     * @return array List of page IDs.
+     */
+    private static function _get_active_companies()
+    {
+        global $wpdb;
+
+        $page_ids = wp_cache_get('all_company_ids', 'posts');
+        if (! is_array($page_ids)) {
+            $page_ids = $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE post_type = 'company' AND post_status = 'publish'");
+            wp_cache_add('all_company_ids', $page_ids, 'posts');
+        }
+
+        return $page_ids;
     }
 }
