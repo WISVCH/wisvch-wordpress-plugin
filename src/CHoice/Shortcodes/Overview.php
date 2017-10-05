c3c90b150e65d9a6c880af278ea37c4cbd5aa565
Sven Popping <sven@popping.co>
2017-10-05 15:52:13 +0200

    Remove unnecessary code

--- src/CHoice/Shortcodes/Overview.php
+++ src/CHoice/Shortcodes/Overview.php
@@ -23,27 +23,6 @@ class Overview extends Template
 
         $return_data = parent::getTemplateData();
 
-        // Return data
-        $return_data['greeting'] = self::_greeting();
-
         return $return_data;
-    }
-
-    private static function _greeting()
-    {
-
-        $hour = date('G');
-
-        if ($hour >= 0 && $hour < 6) {
-            $greeting = 'Good night';
-        } elseif ($hour < 12) {
-            $greeting = 'Good afternoon';
-        } elseif ($hour < 18) {
-            $greeting = 'Good afternoon';
-        } else {
-            $greeting = 'Good evening';
-        }
-
-        return $greeting;
     }
 }
